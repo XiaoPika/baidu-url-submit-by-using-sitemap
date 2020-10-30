e21a24294a0667cae0fa87ee1a6a0a5f28d5e4ae
小屁卡 <admin@pika666.cn>
2020-10-30 17:25:12 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import re
 import urllib
 import requests
 
-sitemap = 'https://blog.pika666.cn/search.xml'
+sitemap = 'https://blog.pika666.cn/baidusitemap.xml'
 
 html = urllib.request.urlopen(sitemap).read().decode('utf-8')
 result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
